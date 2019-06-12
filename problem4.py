b151808ae0e9d115785929a0f2b306ef0dd35661
mohit266 <16tec2cs012@vgu.ac.in>
2019-06-13 00:20:48 +0530

    .

---
+++ problem4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import subprocess
+import os
+import crypt
+user=input("Enter the user name:")
+if user.isalpha()==True:
+  print( "Enter username :" ,user)
+else:
+  print("Enter correct username")	
+
+password="hello"+user
+encryptpassword = crypt.crypt(password,"22") 
+subprocess.call("useradd -p " + encryptpassword +" "+user, shell=True)
