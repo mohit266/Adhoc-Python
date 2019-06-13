0ef031797e5e54e3d9b7a2c6783685cea9ddc5de
Mohit suthar <16tec2cs012@vgu.ac.in>
2019-06-14 00:40:16 +0530

    Update problem5.py

--- problem5.py
+++ problem5.py
@@ -7,7 +7,7 @@ current_time = datetime.datetime.now()
 
 if current_time.hour < 12:
    print('Good morning',name)
-elif 12 <= current_time.hour < 17:
+elif 12 <= current_time.hour < 16:
    print('Good afternoon',name)
 elif 16 <= current_time.hour <21:
    print('Good evening',name)
